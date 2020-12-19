37502879e34a298b473274c602d8eb7dcc0b8097
theethaj <bank30827@gmail.com>
2020-12-19 19:39:46 +0700

    Create a component for the Set username screen

---
+++ gyo-nuxt/components/UsenameForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id='set-user-name'>
+    <h1>Set username</h1>
+    <div id='username-form'>
+      <b-nav-form>
+        <b-form-input class="mr-sm-2" placeholder="Username"></b-form-input>
+        <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Enter</b-button>
+      </b-nav-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ gyo-nuxt/pages/set-username.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id='set-username'>
+    <nav-bar></nav-bar>
+    <b-container>
+      <usename-form></usename-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import UsenameForm from '~/components/UsenameForm.vue'
+export default {
+  components: { UsenameForm }
+}
+</script>
+
+<style>
+
+</style>
