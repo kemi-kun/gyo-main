f04f2f436788028427828eb2eea7805e7b6d8cf1
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-20 01:20:58 +0700

    Create a component for the Room settings screen

---
+++ gyo-nuxt/components/RoomSettingForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id='room-setting-form'>
+    <h1>Setting</h1>
+    <div id='form-fields'>
+      <b-form>
+        <label class="mr-sm-2" for="inline-form-set-timmer">Timer</label>
+        <b-form-input id="inline-form-set-timmer" class="mb-2 mr-sm-2 mb-sm-0" placeholder="10"></b-form-input>
+        <label class="mr-sm-2" for="inline-form-set-num_words"># words</label>
+        <b-form-input id="inline-form-set-num_words" class="mb-2 mr-sm-2 mb-sm-0" placeholder="15"></b-form-input>
+        <b-button variant="primary" type='submit'>Save</b-button>
+      </b-form>
+      <b-button variant="secondary">Back</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ gyo-nuxt/pages/room-setting.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id='room-setting'>
+    <nav-bar></nav-bar>
+    <room-setting-form></room-setting-form>
+  </div>
+</template>
+
+<script>
+import RoomSettingForm from '~/components/RoomSettingForm.vue'
+import NavBar from '~/components/NavBar.vue'
+export default {
+  components: {
+    RoomSettingForm,
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
