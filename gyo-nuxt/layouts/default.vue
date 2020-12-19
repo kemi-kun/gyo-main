ac97599a0bf6940a9cb1bc464da9cae382fd50da
T. Bunratta <tharathorn.bu@ku.th>
2020-12-19 18:39:42 +0700

    Add checkout step

--- gyo-nuxt/layouts/default.vue
+++ gyo-nuxt/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Navbar></Navbar>
     <Nuxt />
   </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 html {
