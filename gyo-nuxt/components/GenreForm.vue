0b466b6408fcf206aa1ed9f9fe98ba1e5a60c058
theethaj <bank30827@gmail.com>
2020-12-19 19:42:28 +0700

    Create a component for the Select genre screen

---
+++ gyo-nuxt/components/GenreForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='genre-form'>
+    <h1>Select the genre</h1>
+    <a href=''>Change setting</a>
+    <b-nav-form>
+      <b-form-input class="mr-sm-2" placeholder="Username"></b-form-input>
+      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Enter</b-button>
+    </b-nav-form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ gyo-nuxt/pages/select-genre.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id='select-genre'>
+    <nav-bar></nav-bar>
+    <b-container>
+      <genre-form></genre-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+import GenreForm from '~/components/GenreForm.vue'
+export default {
+  components: {
+    NavBar,
+    GenreForm
+  }
+}
+</script>
+
+<style>
+
+</style>
