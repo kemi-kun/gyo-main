05098e173ab140c10ff372c7d57c23a1cfeb43a0
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-20 12:19:58 +0700

    Create select join component for join game

---
+++ gyo-nuxt/components/SelectJoin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav-form>
+      <b-form-input class="mr-sm-2" placeholder="Gamepin"></b-form-input>
+      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit" href='set-username'>Join</b-button>
+    </b-nav-form>
+    <b-button id='create-mode' size='lg' href='#'>Create</b-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
