53b06c43363877b1a75cf741c1ba3f8178e121c0
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-20 12:26:05 +0700

    Create word answer for start round

---
+++ gyo-nuxt/components/WordAnswer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="word-answer">
+    <challenge-word :cword="tempCword"></challenge-word>
+    <b-list-group id="answer-songs">
+      <b-list-group-item v-for="song in songs" :id="song.id" :key="song.id">
+        <p>{{ song.name }}</p>
+        <p>By {{ song.artist }}</p>
+      </b-list-group-item>
+    </b-list-group>
+    <b-button id='next-button' size='lg' href='#'>Next word</b-button>
+  </div>
+</template>
+
+<script>
+import ChallengeWord from '~/components/ChallengeWord.vue'
+export default {
+  components: { ChallengeWord },
+  props: {
+    cword: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      songs: [
+        { id: 1, name: 'Song 1', artist: 'Artist' },
+        { id: 2, name: 'Song 2', artist: 'Artist 2' }
+      ],
+      tempCword: this.cword
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- gyo-nuxt/pages/round/_id/start-round.vue
+++ gyo-nuxt/pages/round/_id/start-round.vue
@@ -3,6 +3,7 @@
     <wait-round></wait-round>
     <word-countdown cword='Hate'></word-countdown>
     <enter-answer-form cword='Hate'></enter-answer-form>
+    <word-answer cword='Hate'></word-answer>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import WaitRound from '~/components/WaitRound.vue'
 import WordCountdown from '~/components/WordCountdown.vue'
 import EnterAnswerForm from '~/components/EnterAnswerForm.vue'
+import WordAnswer from '~/components/WordAnswer.vue'
 export default {
   components: {
     WaitRound,
     WordCountdown,
-    EnterAnswerForm
+    EnterAnswerForm,
+    WordAnswer
   }
 }
 </script>
