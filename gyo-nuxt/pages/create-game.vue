4b124b2fe146612a30f1f287d8ac677c439a7aea
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-20 12:17:32 +0700

    Create game form for create game page

---
+++ gyo-nuxt/pages/create-game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id='create-game'>
+    <nav-bar></nav-bar>
+    <b-container>
+      <create-game-form></create-game-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+import CreateGameForm from '~/components/CreateGameForm.vue'
+export default {
+  components: {
+    CreateGameForm,
+    NavBar
+  }
+}
+</script>
+
+<style>
+</style>
