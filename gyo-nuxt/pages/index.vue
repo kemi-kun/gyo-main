17f11c7e29ebe2b18ac5d2b06f5dd04ba9404a66
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-21 19:14:50 +0700

    Setup nightwatch for e2e testing
    - Add nightwatch config file
    - Write an example test to test the provided `index.vue`
    - Create a dummy page `page1.vue` for testing the example test

--- gyo-nuxt/pages/index.vue
+++ gyo-nuxt/pages/index.vue
@@ -7,20 +7,12 @@
       </h1>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
+          id="page-1"
+          href="page1"
           rel="noopener noreferrer"
           class="button--grey"
         >
-          GitHub
+          Page 1
         </a>
       </div>
     </div>
